1cf8d8d8309d5d200142b8ab72ab51a90f285ac0
Doug Weitner <dweitner@squarespace.com>
2020-11-11 17:50:36 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,50 +1,43 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import React, { useState } from "react";
+import "./App.css";
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
+function LambdaDemo() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState("");
+
+  function openDoor() {
+    setIsLoading(true);
+    fetch("/.netlify/functions/async-dadjoke")
+      .then((response) => response.json())
+      .then((data) => {
+        setIsLoading(false);
+        console.log(data);
+        setData(data.msg);
+      });
   }
 
-  handleClick = api => e => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
+  return (
+    <p>
+      <button onClick={openDoor}>
+        {isLoading ? "Loading..." : "Call Async Lambda"}
+      </button>
+      <br />
+      <span>{data}</span>
+    </p>
+  );
 }
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
-      </div>
-    )
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Click to open
+        </p>
+        <LambdaDemo />
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
