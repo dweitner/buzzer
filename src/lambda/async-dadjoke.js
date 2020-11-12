47aa49ae7b8184a8a690e2c8ecf49279e4d35c4b
Doug Weitner <dweitner@squarespace.com>
2020-11-12 00:32:38 -0500

    update async to post

--- src/lambda/async-dadjoke.js
+++ src/lambda/async-dadjoke.js
@@ -1,20 +1,20 @@
 // example of async handler using async-await
 // https://github.com/netlify/netlify-lambda/issues/43#issuecomment-444618311
 
-import axios from "axios"
+import axios from "axios";
 export async function handler(event, context) {
   try {
-    const response = await axios.get("http://74.71.87.249/")
-    const data = response.data
+    const response = await axios.post("http://74.71.87.249/open");
+    const data = response.data;
     return {
       statusCode: 200,
-      body: JSON.stringify({ msg: data })
-    }
+      body: JSON.stringify({ msg: data }),
+    };
   } catch (err) {
-    console.log(err) // output to netlify function log
+    console.log(err); // output to netlify function log
     return {
       statusCode: 500,
-      body: JSON.stringify({ msg: err.message }) // Could be a custom message or object i.e. JSON.stringify(err)
-    }
+      body: JSON.stringify({ msg: err.message }), // Could be a custom message or object i.e. JSON.stringify(err)
+    };
   }
 }
